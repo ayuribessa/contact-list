504df9a3aa2f3d679ab4e03e53446d729386b645
Ayuri Bessa <ayuribessa@gmail.com>
2022-02-02 16:00:31 -0300

    fix null insert on models

--- db/migrate/20220202142111_create_contacts.rb
+++ db/migrate/20220202142111_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration[7.0]
   def change
     create_table :contacts do |t|
-      t.string :name
+      t.string :name, null: false
       t.date :birthday
       t.references :user, null: false, foreign_key: true
 
--- db/migrate/20220202142636_create_telephones.rb
+++ db/migrate/20220202142636_create_telephones.rb
@@ -1,9 +1,9 @@
 class CreateTelephones < ActiveRecord::Migration[7.0]
   def change
     create_table :telephones do |t|
-      t.string :number
-      t.string :type
-      t.boolean :main
+      t.string :number, null: false
+      t.string :type, null: false
+      t.boolean :main, null: false
       t.references :contact, null: false, foreign_key: true
 
       t.timestamps
