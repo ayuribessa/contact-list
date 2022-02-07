48b0f064a96b686ece2cbb71173b994d7848735c
Ayuri Bessa <ayuribessa@gmail.com>
2022-02-07 16:16:40 -0300

    add rspec testes for user model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  it "is not valid without name" do
+    user = User.new(name: nil)
+    expect(user).to_not be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without name" do
+    user = User.create(name: nil,birthday:"09/03/1994", bio:"my bio!")
+    user.valid?   
+    expect(user.errors[:name]).to include("can't be blank") 
+  end
+  
+  it "Is valid when has atribute name, birthday and bio" do
+    user = User.create!(name:"Ayuri Bessa",birthday:"09/03/1994", bio:"my bio!")
+    expect(user).to be_valid
+  end
+    
+  it "does not create if the user name is already taken" do
+    User.create!(name:"Ayuri Bessa",birthday:"09/03/1995", bio:"my alt bio!")
+    expect(User.count).to eq 1
+  end
 end
