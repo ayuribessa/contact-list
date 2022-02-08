89ad47abaa94cbdf42f96571f2aa3f6ef17a3816
Ayuri Bessa <ayuribessa@gmail.com>
2022-02-08 06:28:30 -0300

    update fixtures

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  fixtures :users
   it "is not valid without name" do
-    fixtures :contacts
-    # user = User.create(name: nil,birthday:"09/03/1994", bio:"my bio!")
-    user = users(:one)
+    user = User.create(name: nil,birthday:"09/03/1994", bio:"my bio!")
     user.valid?   
     expect(user.errors[:name]).to include("can't be blank") 
   end
@@ -15,8 +14,7 @@ RSpec.describe User, type: :model do
   end
     
   it "does not create if the user name is already taken" do
-    user = User.create(name:"Ayuri Bessa",birthday:"09/03/1994", bio:"my bio!")
-    user = User.create(name:"Ayuri Bessa",birthday:"09/03/1995", bio:"my alt bio!")
+    user = User.create(name:"Fulano",birthday:"09/03/1994", bio:"my bio!")
     user.valid? 
     expect(user.errors[:name]).to include('has already been taken')
   end
