15207e2220b478eb649c925727986eb72115895d
Ayuri Bessa <ayuribessa@gmail.com>
2022-02-11 16:13:52 -0300

    teste

--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -3,5 +3,12 @@ class Telephone < ApplicationRecord
   validates :number, presence: true, uniqueness: {scope: :contact_id}
   validates :kind, presence: true, inclusion: ['home','work', 'other']
   validates :main, inclusion:{ in: [true, false] }, uniqueness: {conditions: -> {where(main: true)}}
+  before_validation :turn_old_main_to_false
 
+  private 
+  def turn_old_main_to_false
+    return unless :main?
+    self.main = false if :main?
+  end
+  
 end
