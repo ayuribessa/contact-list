8504136b58f3d34b3c74bb93d7eea2753177fbbd
Ayuri Bessa <ayuribessa@gmail.com>
2022-02-16 11:16:12 -0300

    add users lists, view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users' => 'users#index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
